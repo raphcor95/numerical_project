72e89080c57573261f574c2828be2861f68a6eb7
Romain Pifaut <romain.pifaut@gmail.com>
2025-05-02 15:48:58 +0200

    Creating Underlying and Basket classes

---
+++ Processes/Underlyings/Underlying.cpp
@@ -0,0 +1,21 @@
+#include "Underlying.h"
+
+// Constructor
+Underlying::Underlying(RandomProcess* generator, 
+                            double initVal, const std::vector<double>& vecWeights):
+    Generator(generator), InitVal(initVal), VecWeights(vecWeights)
+{
+
+}
+
+// Destructor
+Underlying::~Underlying()
+{
+
+}
+
+// Wrapper for simulation function
+void Underlying::Simulate(double startTime, double endTime, size_t nbSteps)
+{
+
+}
---
+++ Processes/Underlyings/Underlying.h
@@ -0,0 +1,15 @@
+#include "RandomProcess.h"
+
+class Underlying
+{
+    protected:
+        RandomProcess* Generator;
+        SinglePath* Path;
+        double InitVal;
+        std::vector<double> VecWeights;
+
+    public:
+        Underlying(RandomProcess* generator, double initVal, const std::vector<double>& vecWeights);
+        ~Underlying();
+        virtual void Simulate(double startTime, double endTime, size_t nbSteps) = 0;
+};
