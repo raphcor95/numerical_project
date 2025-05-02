453cbf70f117f1c4762d5d1a3648f7abee114ebc
Romain Pifaut <romain.pifaut@gmail.com>
2025-05-02 11:53:58 +0200

    Renaming MatCorr to MatCov

--- Processes/BlackScholesND.cpp
+++ Processes/BlackScholesND.cpp
@@ -1,20 +1,63 @@
 #include "BlackScholesND.h"
+#include "../Utils/Matrix.h"
 #include <vector>
+#include <string>
+#include <iostream>
 
 BlackScholesND::BlackScholesND(Normal* Gen,
-    std::vector<double>& vecSpots,
-    std::vector<double>& vecRates,
-    std::vector<std::vector<double>>& matCorr
+    const std::vector<double>& vecSpots,
+    const std::vector<double>& vecRates,
+    Matrix* matCov
 ) : 
     RandomProcess(Gen, vecSpots.size()),
     VecSpots(vecSpots),
     VecRates(vecRates),
-    MatCorr(matCorr)
+    MatCov(matCov)
 {
+    // Check for the validity of the inputs
+    if (vecSpots.size() != vecRates.size())
+    {
+        throw runtime_error("Error: Lengths must match. vecSpots has length: " 
+                            + std::to_string(vecSpots.size()) 
+                            + " and vecRates has length " + std::to_string(vecRates.size()) + "."
+        );
+    } 
+    
+    if (MatCov->getRows() != MatCov->getCols())
+    {
+        throw runtime_error("Error: Correlation matrix must be a square matrix. Dimensions are" 
+                            + std::to_string(MatCov->getRows())
+                            + "x"
+                            + std::to_string(MatCov->getCols())
+        );
+    }
+
+    if (vecSpots.size() != MatCov->getRows())
+    {
+        throw std::runtime_error("Error: Length of the vecSpots vector "
+                                + std::to_string(vecSpots.size())
+                                + " must match the correlation matrix dimensions "
+                                + std::to_string(MatCov->getRows())
+                                + " square matrix."
+        );
+    }
+
+    if (vecRates.size() != MatCov->getRows())
+    {
+        throw std::runtime_error("Error: Length of the vecRates vector "
+                                + std::to_string(vecRates.size())
+                                + " must match the correlation matrix dimensions "
+                                + std::to_string(MatCov->getRows())
+                                + " square matrix."
+        );
+    }
+
+
+    
 
 }
 
 BlackScholesND::~BlackScholesND()
 {
-    
+
 }
--- Processes/BlackScholesND.h
+++ Processes/BlackScholesND.h
@@ -1,19 +1,20 @@
 #pragma once
 #include <vector>
 #include "RandomProcess.h"
+#include "../Utils/Matrix.h"
 
 class BlackScholesND : public RandomProcess
 {
     protected:
-        std::vector<double> VecSpots;
-        std::vector<double> VecRates;
-        std::vector<std::vector<double>> MatCorr;
+        const std::vector<double>& VecSpots;
+        const std::vector<double>& VecRates;
+        Matrix* MatCov;
 
     public:
         BlackScholesND(Normal* Gen, 
-                        std::vector<double>& vecSpots,
-                        std::vector<double>& vecRates,
-                        std::vector<std::vector<double>>& matCorr);
+                        const std::vector<double>& vecSpots,
+                        const std::vector<double>& vecRates,
+                        Matrix* matCov);
         ~BlackScholesND();
 };
 
