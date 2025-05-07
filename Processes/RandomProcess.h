2f1d67e0158d62840da1adade133e92f1a6c1696
Romain Pifaut <romain.pifaut@gmail.com>
2025-05-07 21:30:23 +0200

    Adding new function signature for the random Simulate method -> for anthitetic random variables

--- Processes/RandomProcess.h
+++ Processes/RandomProcess.h
@@ -14,7 +14,7 @@ public:
 	RandomProcess(RandomGenerator* Gen, int dim);
 	virtual void Simulate(double startTime, double endTime, size_t nbSteps) = 0;
 	virtual void Simulate(double startTime, double endTime, size_t nbSteps, 
-							std::vector< std::vector<double> >* vecW_anti) = 0;
+							std::vector< std::vector<double> >* vecW_anti = nullptr) = 0;
 	SinglePath* GetPath(int dimension);
 	~RandomProcess();
 };
