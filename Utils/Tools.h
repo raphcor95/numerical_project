013111e0f12ab1db96bdcfa5517abe430435892b
Romain Pifaut <romain.pifaut@gmail.com>
2025-05-08 13:06:39 +0200

    Adding recursive bAdic expansion computation (Paul Glasserman) + Added vector of prime numbers

--- Utils/Tools.h
+++ Utils/Tools.h
@@ -1,11 +1,20 @@
 #pragma once
 #include "../Processes/RandomProcess.h"
-
+#include <deque>
 #include <iostream>
 
 std::vector<std::vector<std::vector<double>>> generate_antithetic(Normal* Gen, size_t dim_sim, size_t dim_steps, size_t dim_undl);
 
-/* Tool functions for the generation of the Quasi Random Numbers */
 
+/* Tool functions for the generation of the Quasi Random Numbers */
+// The optimised algorithms (especially the recursive one were proposed by Paul Glasserman)
 // B-Adic Decomposition of a positive integer
-std::vector<int> bAdic(int k, int b);
+std::deque<int> bAdic(int k, int b);
+
+// Recursive B-Adic Decomposition of a positive integer
+std::deque<int>& RecusriveBAdic(std::deque<int>& vecDecompo, int b);
+
+
+
+/* 500 first prime numbers for Quasi Random Numbers up to 500 dimensional vectors */
+extern std::vector<int> primes;
