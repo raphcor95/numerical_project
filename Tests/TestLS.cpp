5fdad1c65942e15ac175d7e8060d5b09dbb06bde
Romain Pifaut <romain.pifaut@gmail.com>
2025-05-10 22:44:01 +0200

    update test_ls

--- Tests/TestLS.cpp
+++ Tests/TestLS.cpp
@@ -3,12 +3,16 @@
 #include "../RandomGenerators/UniformGenerators/LinearCongruential.h"
 #include "../Processes/BSEuler1D.h"
 #include "../Processes/BSEulerND.h"
+#include "../Processes/BSEulerNDAnti.h"
 #include "../Processes/Underlyings/Basket.h"
 #include "../Pricers/LongstaffSchwarz/LongstaffSchwarz.h"
 #include "../Pricers/LongstaffSchwarz/LSLaguerrePoly.h"
+#include "../Pricers/LongstaffSchwarz/LSStandard.h"
 #include "../Payoffs/EuropeanCall.h"
+#include "../Payoffs/EuropeanPut.h"
 #include "../Utils/Input.h"
 #include "../Utils/Output.h"
+#include "../Utils/Tools.h"
 
 int main()
 {
@@ -16,11 +20,12 @@ int main()
         // Variables
         double rate = 0.05;
         double vol = 0.1;
-        double spot = 1;
+        double spot = 100;
+        double strike = 85.0;
         double startTime = 0;
         double endTime = 1;
-        double nbSim = 1000;
-        size_t nbSteps = 1000;
+        double nbSim = 100;
+        size_t nbSteps = 100;
 
         // Generators
         UniformGenerator* Unif = new EcuyerCombined();
@@ -28,20 +33,30 @@ int main()
 
         // Asset Dynamics Parameters - Basket Case
         double N = 3.0;
-        std::vector<double> vecSpots(3, 1.0);
-        std::vector<double> vecRates(3, 0.05);
+        std::vector<double> vecSpots(3, spot);
+        std::vector<double> vecRates(3, rate);
         std::vector<double> vecWeights = {1.0, 0.0, 0.0};
         Input* Inp = new Input();
-        Matrix* matCov = Inp->CSV2Mat("Inputs/matCovIndep.csv");
+        Matrix* matCov = Inp->CSV2Mat("Inputs/matCov.csv");
+
+        // Control Variate
+        bool ControlVariate = false;
 
         // MultiAsset Path Generator
-        RandomProcess* BSEulerMulti = new BSEulerND(Norm, vecSpots, vecRates, matCov);
+        // RandomProcess* BSEulerMulti = new BSEulerND(Norm, vecSpots, vecRates, matCov);
+        RandomProcess* BSEulerMulti = new BSEulerNDAnti(Norm, vecSpots, vecRates, matCov);
         Underlying* myBasket = new Basket(BSEulerMulti, vecSpots[0], vecWeights);
 
         // Longstaff Schwarz Pricer - Laguerre Polynomials
         std::vector<double> vecTimes = {0.2, 0.4, 0.6, 1.0};
-        LongstaffSchwarz* PricerLS = new LSLaguerrePoly(myBasket, vecTimes, nbSim,
-                                    startTime, endTime, nbSteps);
+        // std::vector<double> vecTimes = {1.0};
+        int order = 5;
+        // LongstaffSchwarz* PricerLS = new LSLaguerrePoly(myBasket, vecTimes, nbSim,
+        //                             startTime, endTime, nbSteps);
+        LongstaffSchwarz* PricerLS = new LSStandard(myBasket, vecTimes, nbSim,
+                                        startTime, endTime, nbSteps, order, rate);
+
+
         PricerLS->Simulate();
         const std::vector<SinglePath*>& vecPaths = PricerLS->ReturnPaths();
 
@@ -58,9 +73,13 @@ int main()
         std::cout << "Outputting the results in: Outputs/LSBasket_Simulations.csv" << std::endl;
         
         // Payoff
-        Payoff* EUCall = new EuropeanCall(1.0);
-        double price = PricerLS->Price(EUCall);
-        std::cout << "BS Price of the Bermudan Call Option: " << price << std::endl;
+        Payoff* payoff = new EuropeanCall(strike);
+        // Payoff* payoff = new EuropeanPut(strike);
+        double BSPrice = BSCall(spot, strike, vol, rate, endTime);
+        // double BSPrice = BSPut(spot, strike, vol, rate, endTime);
+        double price = PricerLS->Price(payoff, ControlVariate);
+        std::cout << "BS Price of the Bermudan Option: " << price << std::endl;
+        std::cout << "BS Price of the European Option: " << BSPrice << std::endl;
 
 
 
